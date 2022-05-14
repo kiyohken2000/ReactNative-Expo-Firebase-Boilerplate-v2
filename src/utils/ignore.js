3f07965ef244ae25e80d89c41d8f6c4a9b5a77b0
*D8uZD3ENJO <shirakura@colors.jp>
2022-05-14 15:09:38 +0900

    [fix]warning

--- src/utils/ignore.js
+++ src/utils/ignore.js
@@ -1,4 +1,10 @@
 import { LogBox } from 'react-native'
+import ignoreWarnings from 'ignore-warnings';
+
+ignoreWarnings('warn',['ViewPropTypes','[react-native-gesture-handler]'])
 
 // eslint-disable-next-line no-unused-expressions
-LogBox?.ignoreLogs(['Remote debugger'])
+LogBox?.ignoreLogs([
+  'ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from \'deprecated-react-native-prop-types\'.',
+  'Remote debugger'
+])
