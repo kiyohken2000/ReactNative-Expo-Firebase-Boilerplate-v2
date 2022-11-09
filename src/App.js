dc0324f7d680134818167d230e28bdc0554c5f79
kiyohken2000 <www.broadwell.com@gmail.com>
2022-11-09 10:24:42 +0900

    [upgrade]Expo47

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ import { imageAssets } from 'theme/images'
 import { fontAssets } from 'theme/fonts'
 import Router from './routes'
 
+const isHermes = () => !!global.HermesInternal;
+
 const App = () => {
   // state
   const [didLoad, setDidLoad] = useState(false)
+  console.log('isHermes', isHermes())
 
   // handler
   const handleLoadAssets = async () => {
